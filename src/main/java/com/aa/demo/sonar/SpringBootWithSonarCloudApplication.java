b36e627ac7784f6c09760f904410907dea05c3a3
Murali Seelam <muralidhara.seelam@gmail.com>
2022-04-17 20:40:30 +0530

    test commit1

---
+++ src/main/java/com/aa/demo/sonar/SpringBootWithSonarCloudApplication.java
@@ -0,0 +1,13 @@
+package com.aa.demo.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSonarCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSonarCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aa/demo/sonar/SpringBootWithSonarCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa.demo.sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithSonarCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
